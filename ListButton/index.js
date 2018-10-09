a89c2138c67f7e6377185f7637befb24351b908c
dana <dana.arbel1@gmail.com>
2018-10-08 22:12:16 -0700

    List

---
+++ ListButton/index.js
@@ -0,0 +1,25 @@
+window.onload = () => 
+{  const userInput = document.getElementById('list-data');    
+const setTextButton = document.getElementById('set-text');
+}
+
+setTextButton.onclick = () => {
+    userInput.value = 'The first item for my list';
+ }
+
+ window.onload = () => {
+    const userInput = document.getElementById('list-data');    
+    const setTextButton = document.getElementById('set-text');
+
+    setTextButton.onclick = () => {
+       userInput.value = 'The first item for my list';
+    }
+}
+const elfCode = {
+    appendToList: (list, value) => {
+        const li = document.createElement("li");
+        li.appendChild(document.createTextNode(value));
+        list.appendChild(li);
+    }
+
+}
