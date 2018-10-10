280f806a2af1bc01a60c1ae19718591990ceee88
dana <dana.arbel1@gmail.com>
2018-10-10 12:38:13 -0700

    fixing tags

--- ListButton/elf-code.js
+++ ListButton/elf-code.js
@@ -1,22 +1,10 @@
 
 
 window.onload = () => {
-// Part I here
+    const userInput = document.getElementById('list-data');    
     const setTextButton = document.getElementById('set-text');
-    const userInput = document.getElementById('list-data');
-    const listButton = document.getElementById('add-to-list');
-    
- // Part II here
-const elfCode = {
-    appendToList: (list, value) => {
-        const li = document.createElement("li");
-        li.appendChild(document.createTextNode(value));
-        list.appendChild(li);
+
+    setTextButton.onclick = () => {
+       userInput.value = 'The first item for my list';
     }
-}
-    // Part III here
-listButton.onclick = () => {
-    const myList = document.getElementById('my-list');
-    elfCode.appendToList(myList, userInput.value);
-}
 }
