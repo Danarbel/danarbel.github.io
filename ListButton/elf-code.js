2b988c883811d3926bece9f16cebd6abc7b74a75
dana <dana.arbel1@gmail.com>
2018-10-09 21:56:49 -0700

    fixed list

--- ListButton/elf-code.js
+++ ListButton/elf-code.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-
+// Part I here
     const setTextButton = document.getElementById('set-text');
     const userInput = document.getElementById('list-data');
    
@@ -8,12 +8,13 @@ const elfCode = {
     appendToList: (list, value) => {
         const li = document.createElement("li");
         li.appendChild(document.createTextNode(value));
-        list.appendChild(li);
+        li.appendChild(li);
     }
 }
+    // Part III here
 
 
-listButton.onclick = (list) => {
+listButton.onclick = () => {
     const myList = document.getElementById('my-list');
     elfCode.appendToList(myList, userInput.value);
 }
