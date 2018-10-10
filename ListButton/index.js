11a6c95d6531d977c9c2063d37e54c86eb724a18
dana <dana.arbel1@gmail.com>
2018-10-10 12:36:25 -0700

    fixing tags

--- ListButton/index.js
+++ ListButton/index.js
@@ -1,8 +1,21 @@
 window.onload = () => {
-    const userInput = document.getElementById('list-data');    
-    const setTextButton = document.getElementById('set-text');
-
-    setTextButton.onclick = () => {
-       userInput.value = 'The first item for my list';
+    // Part I here
+        const setTextButton = document.getElementById('set-text');
+        const userInput = document.getElementById('list-data');
+        const listButton = document.getElementById('add-to-list');
+        
+     // Part II here
+    const elfCode = {
+        appendToList: (list, value) => {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(value));
+            list.appendChild(li);
+        }
     }
-}
+        // Part III here
+    listButton.onclick = () => {
+        const myList = document.getElementById('my-list');
+        elfCode.appendToList(myList, userInput.value);
+    }
+    }
+    
