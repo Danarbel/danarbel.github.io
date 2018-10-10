03c52915010e5b02ad8927c0c5fcafa8ead15538
dana <dana.arbel1@gmail.com>
2018-10-09 22:34:06 -0700

    fixing function call to list and file indentation

--- ListButton/elf-code.js
+++ ListButton/elf-code.js
@@ -1,19 +1,19 @@
+
+
 window.onload = () => {
 // Part I here
     const setTextButton = document.getElementById('set-text');
     const userInput = document.getElementById('list-data');
-   
     const listButton = document.getElementById('add-to-list');
+
 const elfCode = {
     appendToList: (list, value) => {
         const li = document.createElement("li");
         li.appendChild(document.createTextNode(value));
-        li.appendChild(li);
+        list.appendChild(li);
     }
 }
     // Part III here
-
-
 listButton.onclick = () => {
     const myList = document.getElementById('my-list');
     elfCode.appendToList(myList, userInput.value);
