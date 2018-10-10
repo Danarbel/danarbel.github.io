e904f6e47718923ffe092c036a8cea4394b3f190
dana <dana.arbel1@gmail.com>
2018-10-10 14:04:33 -0700

    revert

--- ListButton/elf-code.js
+++ ListButton/elf-code.js
@@ -1,22 +1,24 @@
 
 
+
 window.onload = () => {
-// Part I here
-    const setTextButton = document.getElementById('set-text');
-    const userInput = document.getElementById('list-data');
-    const listButton = document.getElementById('add-to-list');
-    
- // Part II here
-const elfCode = {
-    appendToList: (list, value) => {
-        const li = document.createElement("li");
-        li.appendChild(document.createTextNode(value));
-        list.appendChild(li);
+    // Part I here
+        const setTextButton = document.getElementById('set-text');
+        const userInput = document.getElementById('list-data');
+        const listButton = document.getElementById('add-to-list');
+        
+     // Part II here
+    const elfCode = {
+        appendToList: (list, value) => {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(value));
+            list.appendChild(li);
+        }
     }
-}
-    // Part III here
-listButton.onclick = () => {
-    const myList = document.getElementById('my-list');
-    elfCode.appendToList(myList, userInput.value);
-}
-}
+        // Part III here
+    listButton.onclick = () => {
+        const myList = document.getElementById('my-list');
+        elfCode.appendToList(myList, userInput.value);
+    }
+    }
+    
