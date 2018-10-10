c7f0d3474e06f89ecaa787d58b74dbee5cf9f718
dana <dana.arbel1@gmail.com>
2018-10-10 13:23:49 -0700

    fixing index page

--- ListButton/elf-code.js
+++ ListButton/elf-code.js
@@ -1,12 +1,12 @@
 
 
 window.onload = () => {
-
+// Part I here
     const setTextButton = document.getElementById('set-text');
     const userInput = document.getElementById('list-data');
     const listButton = document.getElementById('add-to-list');
     
-
+ // Part II here
 const elfCode = {
     appendToList: (list, value) => {
         const li = document.createElement("li");
@@ -14,6 +14,9 @@ const elfCode = {
         list.appendChild(li);
     }
 }
-
+    // Part III here
+listButton.onclick = () => {
+    const myList = document.getElementById('my-list');
+    elfCode.appendToList(myList, userInput.value);
 }
-
+}
--- ListButton/index.js
+++ ListButton/index.js
@@ -1,12 +1,10 @@
 window.onload = () => {
-    //part one
     const userInput = document.getElementById('list-data');    
     const setTextButton = document.getElementById('set-text');
-//part two
+
     setTextButton.onclick = () => {
        userInput.value = 'The first item for my list';
     }
-    //part three
 }
 listButton.onclick = () => {
     const myList = document.getElementById('my-list');
