9224682b40e26c8a0967754639202f137d8b2999
dana <dana.arbel1@gmail.com>
2018-10-09 22:06:29 -0700

    Revert "fixing list call in function"
    This reverts commit a41a6d81b7710dfa797acd1b23ed7321d1b90f52.

--- ListButton/elf-code.js
+++ ListButton/elf-code.js
@@ -8,14 +8,14 @@ const elfCode = {
     appendToList: (list, value) => {
         const li = document.createElement("li");
         li.appendChild(document.createTextNode(value));
-        list.appendChild(li);
+        li.appendChild(li);
     }
 }
     // Part III here
 
 
 listButton.onclick = () => {
-    const myList = document.getElementById('list-data');
+    const myList = document.getElementById('my-list');
     elfCode.appendToList(myList, userInput.value);
 }
 }
