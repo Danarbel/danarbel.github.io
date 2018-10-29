6354b7240cb080551f7852e2542315803fa58101
dana <dana.arbel1@gmail.com>
2018-10-29 11:34:15 -0700

    adding pages theme

--- JavaScriptTest/javascript-test/week07-day01.js
+++ JavaScriptTest/javascript-test/week07-day01.js
@@ -1,8 +1,14 @@
 const num = 3;
 const numberstring = '3';
-const rain = true;
+const rain = false;
 
 console.log (num,numberstring);
 console.log(typeof num,typeof numberstring,typeof rain);
 console.log(num + numberstring);
-console.log (typeof num)
+console.log (typeof num);
+
+if (rain===true){
+    console.log('go to the gym');
+} else {
+    console.log ('go for a walk');
+}
