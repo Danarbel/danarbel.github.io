c543ecf18a67b67b8492ddd55a775e05f85b2355
dana <dana.arbel1@gmail.com>
2018-10-29 11:16:10 -0700

    fixing html

---
+++ JavaScriptTest/javascript-test/week07-day01.js
@@ -0,0 +1,8 @@
+const num = 3;
+const numberstring = '3';
+const rain = true;
+
+console.log (num,numberstring);
+console.log(typeof num,typeof numberstring,typeof rain);
+console.log(num + numberstring);
+console.log (typeof num)
