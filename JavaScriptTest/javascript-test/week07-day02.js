71e044a40f9cc1aba1031d28b46f591a4a944ddf
dana <dana.arbel1@gmail.com>
2018-10-31 11:51:36 -0700

    class 10.31

--- JavaScriptTest/javascript-test/week07-day02.js
+++ JavaScriptTest/javascript-test/week07-day02.js
@@ -1,7 +1,34 @@
-let result= 1 + 2 * 3
+var showmessage =(message) => {
 
+    console.log("=============");
+    console.log("====" + message);
+    console.log("===============")
+}
+
+showmessage('operatoe precedence');
+
+let result= 1 + (2 * 3)
 console.log(result);
+
 
 result= (1+2) *3
-
 console.log(result);
+
+showmessage('for loops');
+
+for(let i =10; i<=25; i= i+2){
+    console.log(i)
+}
+
+showmessage('while loops')
+let looper=10;
+while(looper < 15){
+    console.log(looper++);
+}
+
+do {
+    console.log(looper++);
+
+}
+
+while (looper<20);
