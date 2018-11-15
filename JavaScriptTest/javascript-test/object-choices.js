bf5ceb53c3f18c96d78ee79d7c30c02604b93d1d
dana <dana.arbel1@gmail.com>
2018-11-15 14:27:08 -0800

    ObjectChoices

--- JavaScriptTest/javascript-test/object-choices.js
+++ JavaScriptTest/javascript-test/object-choices.js
@@ -1,7 +1,46 @@
-var simpleObject = {
-    sayName : function() {
-        'use strict';
-        console.log('"Simple Object.');
+window.onload = () => {
+    const displayFullName = document.getElementById("fullName");
+    const getFullName = document.getElementById("full-name")
+    getFullName.onclick = () => {
+        displayFullName.textContent = myObject.fullName();
     }
-};
+    const getNamesButton = document.getElementById("get-names");
+    const displayName = document.getElementById("name");
+    getNamesButton.onclick = () => {
+        displayName.textContent = myObject.getname();
+    }
 
+}
+
+
+
+
+const myObject = {
+    firstName: "Dana",
+    lastName: "Arbel",
+    getname: function() {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+myObject.fullName = function() {
+    return this.firstName + " "  + this.lastName;
+}
+console.log(myObject.firstName + " " + myObject.lastName);
+console.log(myObject.getname());
+console.log(myObject.fullName());
+
+const functionObject = new FunctionObject
+const FunctionObject = () => {
+    console.log("my function object");
+    const getName = () => {
+        return "Function Object";
+
+    }
+    console.log(getName());
+    functionObject.prototype.getFullName = () => {
+        return "hi";
+    }
+}
+functionObject();
+console.log(getFullName());
